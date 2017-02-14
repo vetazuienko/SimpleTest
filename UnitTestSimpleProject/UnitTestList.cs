d44cfe9e12860015d58616dfaf12047bed2f8ec3
perepely <perepelytsia906090@gmail.com>
2017-02-14 20:57:45 +0200

    AddUnitTest

---
+++ UnitTestSimpleProject/UnitTestList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleProject;
+using System.Collections.Generic;
+
+namespace UnitTestSimpleProject
+{
+    [TestClass]
+    public class UnitTestList
+    {
+
+        private MyClass TestClass;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestClass = new MyClass();
+
+
+        }
+
+        [TestMethod]
+        public void TestNotContainNull()
+        {
+            //arrange
+            List<double> vector = new List<double>();
+
+            vector.Add(1);
+            vector.Add(2);
+            vector.Add(3);
+
+            //act
+            List<double> expectedResult = new List<double>();
+
+            expectedResult.Add(2);
+            expectedResult.Add(3);
+            expectedResult.Add(4);
+
+
+            List<double> realResult = TestClass.increment_vector(vector);
+
+            CollectionAssert.AllItemsAreNotNull(realResult);
+
+
+
+        }
+
+        [TestMethod]
+        public void TestCorrectResult()
+        {
+            //arrange
+            List<double> vector = new List<double>();
+
+            vector.Add(1);
+            vector.Add(2);
+            vector.Add(3);
+
+            //act
+            List<double> expectedResult = new List<double>();
+
+            expectedResult.Add(2);
+            expectedResult.Add(3);
+            expectedResult.Add(4);
+
+
+            List<double> realResult = TestClass.increment_vector(vector);
+
+            CollectionAssert.AreEqual(expectedResult, realResult);
+
+
+
+
+
+        }
+
+
+
+        [TestMethod]
+        public void TestTestTypeDouble()
+        {
+            //arrange
+            List<double> vector = new List<double>();
+
+            vector.Add(1);
+            vector.Add(2);
+            vector.Add(3);
+
+            //act
+            List<double> expectedResult = new List<double>();
+
+            expectedResult.Add(2);
+            expectedResult.Add(3);
+            expectedResult.Add(4);
+
+
+            List<double> realResult = TestClass.increment_vector(vector);
+            double x=0;
+
+            CollectionAssert.AllItemsAreInstancesOfType(realResult, x.GetType());
+
+
+
+
+        }
+    }
+}
---
+++ UnitTestSimpleProject/Unittestpromt.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestSimpleProject
+{
+    [TestClass]
+    public class Unittestpromt
+    {
+
+        [TestMethod]
+        public void Testpromt() {
+
+            MyClass TestClass= new MyClass();
+
+               //arrange
+               // public string prompt(string name)
+
+            string Teststring = "Mike";
+
+            string ExpectedTeststring = "Hello, Mike";
+
+            //act
+
+            MyClass testclass = new MyClass();
+
+            string realresult = TestClass.prompt(Teststring);
+
+
+            StringAssert.Equals(ExpectedTeststring, realresult);
+
+
+
+
+
+        }
+
+
+
+        [TestMethod]
+        public void TestHello()
+        {
+
+            MyClass TestClass = new MyClass();
+
+               //arrange
+               // public string prompt(string name)
+
+            string Teststring = "Mike";
+
+            string ExpectedTeststring = "Hello, Mike";
+
+            //act
+
+            MyClass testclass = new MyClass();
+
+            string Realresult = TestClass.prompt(Teststring);
+
+
+            StringAssert.StartsWith(Realresult, "Hello");
+
+
+           
+
+        }
+    }
+}
