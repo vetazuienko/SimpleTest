d144a313a0b7479a4a3d2e56b51e4e0e716932be
perepely <perepelytsia906090@gmail.com>
2017-02-14 20:02:05 +0200

    load

---
+++ SimpleProject/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProject
+{
+    public class MyClass
+    {
+
+        private double increment(double x) {
+            return x + 1;
+        }
+
+        public string prompt(string name) {
+            return "Hello, " + name;
+        }
+
+        public List<double> increment_vector(List<double> vector) {
+
+            List<double> result = new List<double>();
+
+            foreach (double element in vector) {
+                result.Add(increment(element));
+            }
+
+            return result;
+        }
+    }
+}
